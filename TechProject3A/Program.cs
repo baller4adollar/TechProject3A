b85a1ce2711a6cad9696aef612650c6b3ee3cf13
Nikhil Patel <nikhil8@usf.edu>
2020-09-24 13:55:31 -0700

    I added more content for the writeline to make it more appealing and better suited.

--- TechProject3A/Program.cs
+++ TechProject3A/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Programmer: Nikhil Patel
+// Date: 09/24/2020
+// Description: Creating lines of code to tell user to enter their name and spit out a welcome message including the input
+using System;
 
 namespace TechProject3A
 {
@@ -6,7 +9,15 @@ namespace TechProject3A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name, input;
+            int age;
+            Console.WriteLine(" What is your Name? Please enter it below. ");
+            name = Console.ReadLine();
+            Console.WriteLine(" What is your Age? Please enter it below");
+            input = Console.ReadLine();
+            age = Convert.ToInt32(input);
+            Console.WriteLine(" Hello " + name + " It's cool that you are " + age +  " , also USF is #1 "); 
+            Console.Read();
         }
     }
 }
